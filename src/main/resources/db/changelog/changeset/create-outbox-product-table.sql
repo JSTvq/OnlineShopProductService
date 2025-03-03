c042fcfa6d4c7ec79f1e7ede5103daac2bb653a8
JSTvq <kir8530@yandex.ru>
2025-03-03 23:08:06 +0700

    обновление

---
+++ src/main/resources/db/changelog/changeset/create-outbox-product-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS outbox_product (
+    id SERIAL PRIMARY KEY,
+    aggregate_type VARCHAR(255),
+    aggregate_id BIGINT,
+    topic VARCHAR(255),
+    type VARCHAR(255),
+    payload JSONB,
+    status VARCHAR(50)
+);
---
+++ src/main/resources/db/changelog/changeset/create-product-table.sql
@@ -0,0 +1,7 @@
+create table if not exists product (
+    id serial primary key,
+    category varchar(100) not null,
+    name varchar(100) not null,
+    price numeric(19, 2) not null,
+    stock_quantity integer not null
+);
