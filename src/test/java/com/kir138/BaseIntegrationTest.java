3abf92137ade57b42e09a969fe5432b8874cf76f
JSTvq <kir8530@yandex.ru>
2025-03-14 13:03:52 +0700

    обновление

---
+++ src/test/java/com/kir138/BaseIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.kir138;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+    @Container
+    // Автоматическое подключение контейнера
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14.5")
+            .withDatabaseName("testdb")
+            .withUsername("postgres")
+            .withPassword("12341234");
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
---
+++ src/test/java/com/kir138/controller/ProductControllerTest.java
@@ -0,0 +1,24 @@
+package com.kir138.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void saveOrUpdateProduct() {
+    }
+
+    @Test
+    void deleteProductById() {
+    }
+}
